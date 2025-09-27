641260ebd4af62ea7cc821237ca26c6de5f791ac
Manan <manangupta123@gmail.com>
2025-09-27 14:10:21 +0530

    Fix build in linux ami

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
   webpack: (config) => {
+    // Ignore all .d.ts files in node_modules to prevent SWC build errors on Linux
     config.module.rules.push({
       test: /\.d\.ts$/,
       loader: 'ignore-loader',
@@ -9,4 +15,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
